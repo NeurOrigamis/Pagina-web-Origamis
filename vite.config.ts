0c50ac1c9c8cca0ba5ce16511c5467534932ed02
NeurOrigamis <carloscka@gmail.com>
2025-09-21 09:25:18 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/nombre-de-tu-repositorio/',
+  base: '/Pagina-web-Origamis/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
