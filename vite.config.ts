9f31769e1dff62250c527c7e6ffffd732f27a699
NeurOrigamis <carloscka@gmail.com>
2025-11-05 13:29:52 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Pag-na-wen-Origamis/',  // 👈 nombre exacto del repositorio en GitHub
+  base: '/Pag-na-web-Origamis/',  // 👈 nombre exacto del repositorio en GitHub
   build: { outDir: 'docs' },      // 👈 carpeta donde se guardará el build para GitHub Pages
   plugins: [react()],
   optimizeDeps: {
